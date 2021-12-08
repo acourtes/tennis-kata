708263179c234e1adeeaa5822b5d53c919abe8e4
Arnaud Courtès <arnaud@courtes.fr>
2021-12-08 22:52:57 +0100

    Test for game score display when player 1 scores

--- src/main/java/fr/client/TennisGame.java
+++ src/main/java/fr/client/TennisGame.java
@@ -1,7 +1,16 @@
 package fr.client;
 
 public class TennisGame {
+    boolean player1Scored;
+
     public String displayGameScore() {
+        if (player1Scored) {
+            return "Current game status : 15-0";
+        }
         return "Current game status : 0-0";
+    }
+
+    public void player1Scores() {
+        player1Scored = true;
     }
 }
--- src/test/java/fr/client/TennisGameTest.java
+++ src/test/java/fr/client/TennisGameTest.java
@@ -1,16 +1,25 @@
 package fr.client;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TennisGameTest {
+    private final TennisGame sut = new TennisGame();
 
     @Test
     void should_display_0_0_when_there_is_no_point_in_current_game() {
-        var sut = new TennisGame();
+        String result = sut.displayGameScore();
+
+        assertThat(result).isEqualTo("Current game status : 0-0");
+    }
+
+    @Test
+    void should_display_15_0_when_player_1_scores() {
+        sut.player1Scores();
 
         String result = sut.displayGameScore();
 
-        Assertions.assertThat(result).isEqualTo("Current game status : 0-0");
+        assertThat(result).isEqualTo("Current game status : 15-0");
     }
 }
