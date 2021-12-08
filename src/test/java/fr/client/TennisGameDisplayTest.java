561ca239d58acd69a3000f60f53e3da03c8e02ec
Arnaud Courtès <arnaud@courtes.fr>
2021-12-08 23:40:25 +0100

    Added minimal tests for display

---
+++ src/test/java/fr/client/TennisGameDisplayTest.java
@@ -0,0 +1,22 @@
+package fr.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TennisGameDisplayTest {
+
+    @Test
+    void should_display_deuce_when_players_scored_three_times_each() {
+        var result = TennisGameDisplay.displayGameScore(3, 3);
+
+        assertThat(result).isEqualTo("Current game status : deuce");
+    }
+
+    @Test
+    void should_display_15_30_when_player_1_scored_once_and_player_2_scored_twice() {
+        var result = TennisGameDisplay.displayGameScore(1, 2);
+
+        assertThat(result).isEqualTo("Current game status : 15-30");
+    }
+}
