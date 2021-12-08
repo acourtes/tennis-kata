b01b8528a3042a48ad0464ea67ce8fbf72139f98
Arnaud Courtès <arnaud@courtes.fr>
2021-12-08 23:35:07 +0100

    Extracted current game display management in another class

--- src/main/java/fr/client/TennisGame.java
+++ src/main/java/fr/client/TennisGame.java
@@ -1,36 +1,11 @@
 package fr.client;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class TennisGame {
     private int player1Score;
     private int player2Score;
 
-    private static final String CURRENT_GAME_STATUS = "Current game status : ";
-    private final String gameScoreDisplay = "%s-%s";
-    private static final String DEUCE = "deuce";
-
-    private static final Map<Integer, String> pointsToGameScore = new HashMap<>(4);
-
-    static {
-        pointsToGameScore.put(0, "0");
-        pointsToGameScore.put(1, "15");
-        pointsToGameScore.put(2, "30");
-        pointsToGameScore.put(3, "40");
-    }
-
     public String displayGameScore() {
-        return CURRENT_GAME_STATUS + setGameScore();
-    }
-
-    private String setGameScore() {
-        if (player1Score == 3 && player2Score == 3) {
-            return DEUCE;
-        }
-
-        return gameScoreDisplay.formatted(pointsToGameScore.get(player1Score),
-                pointsToGameScore.get(player2Score));
+        return TennisGameDisplay.displayGameScore(player1Score, player2Score);
     }
 
     public void player1Scores() {
---
+++ src/main/java/fr/client/TennisGameDisplay.java
@@ -0,0 +1,32 @@
+package fr.client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class TennisGameDisplay {
+    private static final String CURRENT_GAME_STATUS = "Current game status : ";
+    private static final String gameScoreDisplay = "%s-%s";
+    private static final String DEUCE = "deuce";
+
+    private static final Map<Integer, String> pointsToGameScore = new HashMap<>(4);
+
+    static {
+        pointsToGameScore.put(0, "0");
+        pointsToGameScore.put(1, "15");
+        pointsToGameScore.put(2, "30");
+        pointsToGameScore.put(3, "40");
+    }
+
+    public static String displayGameScore(int player1Score, int player2Score) {
+        return CURRENT_GAME_STATUS + getGameScore(player1Score, player2Score);
+    }
+
+    private static String getGameScore(int player1Score, int player2Score) {
+        if (player1Score == 3 && player2Score == 3) {
+            return DEUCE;
+        }
+
+        return gameScoreDisplay.formatted(pointsToGameScore.get(player1Score),
+                pointsToGameScore.get(player2Score));
+    }
+}
