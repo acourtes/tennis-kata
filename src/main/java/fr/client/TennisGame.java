27e170eb8b43ad61c849f3cf3b1b64a8a169a360
Arnaud Courtès <arnaud@courtes.fr>
2021-12-08 22:58:47 +0100

    Test for game score display 30-0

--- src/main/java/fr/client/TennisGame.java
+++ src/main/java/fr/client/TennisGame.java
@@ -1,16 +1,18 @@
 package fr.client;
 
 public class TennisGame {
-    boolean player1Scored;
+    short player1Score;
 
     public String displayGameScore() {
-        if (player1Scored) {
+        if (player1Score == 1) {
             return "Current game status : 15-0";
+        } else if (player1Score == 2) {
+            return "Current game status : 30-0";
         }
         return "Current game status : 0-0";
     }
 
     public void player1Scores() {
-        player1Scored = true;
+        player1Score++;
     }
 }
--- src/test/java/fr/client/TennisGameTest.java
+++ src/test/java/fr/client/TennisGameTest.java
@@ -1,11 +1,17 @@
 package fr.client;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TennisGameTest {
-    private final TennisGame sut = new TennisGame();
+    private TennisGame sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new TennisGame();
+    }
 
     @Test
     void should_display_0_0_when_there_is_no_point_in_current_game() {
@@ -21,5 +27,15 @@ public class TennisGameTest {
         String result = sut.displayGameScore();
 
         assertThat(result).isEqualTo("Current game status : 15-0");
+    }
+
+    @Test
+    void should_display_30_0_when_player_1_scores_twice() {
+        sut.player1Scores();
+        sut.player1Scores();
+
+        String result = sut.displayGameScore();
+
+        assertThat(result).isEqualTo("Current game status : 30-0");
     }
 }
