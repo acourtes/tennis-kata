54fb45c2952c89dba65c7f397ecdc16e9bbb1cb3
Arnaud Courtès <arnaud@courtes.fr>
2021-12-09 02:17:52 +0100

    Added functional display for game and sets

--- src/main/java/fr/client/TennisMatchManager.java
+++ src/main/java/fr/client/TennisMatchManager.java
@@ -1,11 +1,23 @@
 package fr.client;
 
 public class TennisMatchManager {
+    private static final String SYSTEM_LINE_SEPARATOR = System.getProperty("line.separator");
+
+    private final TennisGame tennisGame;
+    private final TennisSetManager tennisSetManager;
+
+    public TennisMatchManager(TennisGame tennisGame, TennisSetManager tennisSetManager) {
+        this.tennisGame = tennisGame;
+        this.tennisSetManager = tennisSetManager;
+    }
+
     public String displayFullInformation() {
         return """
                 Player 1 : nom du joueur1
-                Player 2 : nom du joueur2
-                Score : (0-0)
-                Current game status : 0-0""";
+                Player 2 : nom du joueur2"""
+                + SYSTEM_LINE_SEPARATOR
+                + tennisSetManager.displaySetScore()
+                + SYSTEM_LINE_SEPARATOR
+                + tennisGame.displayGameScore();
     }
 }
--- src/test/java/fr/client/TennisMatchManagerTest.java
+++ src/test/java/fr/client/TennisMatchManagerTest.java
@@ -8,7 +8,7 @@ public class TennisMatchManagerTest {
 
     @Test
     void should_display_full_information_about_a_beginning_match() {
-        TennisMatchManager sut = new TennisMatchManager();
+        TennisMatchManager sut = new TennisMatchManager(new TennisGame(), new TennisSetManager());
 
         String result = sut.displayFullInformation();
 
@@ -17,5 +17,33 @@ public class TennisMatchManagerTest {
                 Player 2 : nom du joueur2
                 Score : (0-0)
                 Current game status : 0-0""");
+    }
+
+    @Test
+    void should_display_full_information_about_a_match() {
+        var tennisSetManager = new TennisSetManager();
+        var tennisGame = new TennisGame();
+        TennisMatchManager sut = new TennisMatchManager(tennisGame, tennisSetManager);
+
+        tennisGame.player1Scores(1);
+        tennisGame.player2Scores(2);
+
+        tennisSetManager.setNumberOfGamesForPlayer1(5);
+        tennisSetManager.setNumberOfGamesForPlayer2(1);
+        tennisSetManager.setNumberOfGamesForPlayer1(1);
+
+        tennisSetManager.setNumberOfGamesForPlayer1(5);
+        tennisSetManager.setNumberOfGamesForPlayer2(5);
+        tennisSetManager.setNumberOfGamesForPlayer1(2);
+
+        tennisSetManager.setNumberOfGamesForPlayer1(1);
+
+        String result = sut.displayFullInformation();
+
+        assertThat(result).isEqualTo("""
+                Player 1 : nom du joueur1
+                Player 2 : nom du joueur2
+                Score : (6-1) (7-5) (1-0)
+                Current game status : 15-30""");
     }
 }
