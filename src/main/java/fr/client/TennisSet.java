495a710f103b73df6079872cf367e50d73da2db3
Arnaud Courtès <arnaud@courtes.fr>
2021-12-09 01:04:52 +0100

    Added simple case for set display

--- src/main/java/fr/client/TennisSet.java
+++ src/main/java/fr/client/TennisSet.java
@@ -1,7 +1,22 @@
 package fr.client;
 
 public class TennisSet {
+    private static final String SCORE = "Score : ";
+    private static final String SET_FORMAT = "(%d-%d)";
+
+    private int numberOfGamesForPlayer1;
+    private int numberOfGamesForPlayer2;
+
     public String displaySetScore() {
-        return "Score : (0-0)";
+        return SCORE +
+                SET_FORMAT.formatted(numberOfGamesForPlayer1, numberOfGamesForPlayer2);
+    }
+
+    public void setNumberOfGamesForPlayer1(int numberOfGamesForPlayer1) {
+        this.numberOfGamesForPlayer1 = numberOfGamesForPlayer1;
+    }
+
+    public void setNumberOfGamesForPlayer2(int numberOfGamesForPlayer2) {
+        this.numberOfGamesForPlayer2 = numberOfGamesForPlayer2;
     }
 }
--- src/test/java/fr/client/TennisSetTest.java
+++ src/test/java/fr/client/TennisSetTest.java
@@ -1,17 +1,49 @@
 package fr.client;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TennisSetTest {
 
+    private TennisSet sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new TennisSet();
+    }
+
     @Test
     void should_display_0_0_for_first_set_at_the_beginning() {
-        TennisSet sut = new TennisSet();
-
         String result = sut.displaySetScore();
 
         assertThat(result).isEqualTo("Score : (0-0)");
+    }
+
+    @Test
+    void should_display_1_0_when_player_one_won_a_game() {
+        makePlayer1WinGames(1);
+
+        String result = sut.displaySetScore();
+
+        assertThat(result).isEqualTo("Score : (1-0)");
+    }
+
+    @Test
+    void should_display_0_1_when_player_two_won_a_game() {
+        makePlayer2WinGames(1);
+
+        String result = sut.displaySetScore();
+
+        assertThat(result).isEqualTo("Score : (0-1)");
+    }
+
+    private void makePlayer1WinGames(int numberOfGames) {
+        sut.setNumberOfGamesForPlayer1(numberOfGames);
+    }
+
+    private void makePlayer2WinGames(int numberOfGames) {
+        sut.setNumberOfGamesForPlayer2(numberOfGames);
     }
 }
